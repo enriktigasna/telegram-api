7e3d83f59a4a4d9d5298af94b8e2f586e37c4fb5
enriktigasna <maxim.pavlenko2011@gmail.com>
2023-11-22 21:18:32 +0100

    Updated Readme

--- test/client.spec.ts
+++ test/client.spec.ts
@@ -12,34 +12,53 @@ if(process.env.TOKEN) {
 const client = new Client(token);
 const TEST_CHANNEL = process.env.TEST_CHANNEL || -4005456628;
 
-describe("getMe() method", ()=> {
-  it("fetches self user", async()=>{
-    const user = await client.getMe();
-    assert.notEqual(user.id, undefined)
-  })
-})
+describe("Client Class", () => {
+  describe("getMe() method", () => {
+    it("fetches self user", async () => {
+      try {
+        const user = await client.getMe();
+        assert.notEqual(user.id, undefined, "User ID should not be undefined");
+      } catch (error: any) {
+        assert.fail(`Error in getMe test: ${error.message}`);
+      }
+    });
+  });
 
-describe("sendMessage() method", ()=> {
-  it("sends message", async()=>{
-    const testText = "Testing";
-    const message = await client.sendMessage(TEST_CHANNEL, testText);
-    assert.notEqual(message.message_id, undefined)
-    assert.equal(message.text, testText)
-  })
-  it("replies to message", async()=>{
-    const testText = "Testing reply";
-    const message = await client.sendMessage(TEST_CHANNEL, testText, parseInt(process.env.TEST_REPLY_ID || "5"));
+  describe("sendMessage() method", () => {
+    it("sends message", async () => {
+      try {
+        const testText = "Testing";
+        const message = await client.sendMessage(TEST_CHANNEL, testText);
+        assert.notEqual(message.message_id, undefined, "Message ID should not be undefined");
+        assert.equal(message.text, testText, "Message text should match");
+      } catch (error: any) {
+        assert.fail(`Error in sendMessage test: ${error.message}`);
+      }
+    });
 
-    assert.notEqual(message.message_id, undefined)
-    assert.equal(message.text, testText)
-    assert.equal(message.reply_to_message?.message_id, process.env.TEST_REPLY_ID || 5)
-    assert.equal(message.reply_to_message?.chat.id, TEST_CHANNEL)
+    it("replies to message", async () => {
+      try {
+        const testText = "Testing reply";
+        const message = await client.sendMessage(TEST_CHANNEL, testText, parseInt(process.env.TEST_REPLY_ID || "5"));
 
-  })
-  it("sends message despite failed reply, if allow_sending_without_reply=true", async()=>{
-    const testText = "Testing reply";
-    const message = await client.sendMessage(TEST_CHANNEL, testText, 999, true);
-    assert.notEqual(message.message_id, undefined)
-    assert.equal(message.text, testText)
-  })
-})
+        assert.notEqual(message.message_id, undefined, "Message ID should not be undefined");
+        assert.equal(message.text, testText, "Message text should match");
+        assert.equal(message.reply_to_message?.message_id, process.env.TEST_REPLY_ID || 5, "Reply ID should match");
+        assert.equal(message.reply_to_message?.chat.id, TEST_CHANNEL, "Reply channel ID should match");
+      } catch (error: any) {
+        assert.fail(`Error in sendMessage reply test: ${error.message}`);
+      }
+    });
+
+    it("sends message despite failed reply if allow_sending_without_reply", async () => {
+      try {
+        const testText = "Testing reply";
+        const message = await client.sendMessage(TEST_CHANNEL, testText, 999, true);
+        assert.notEqual(message.message_id, undefined, "Message ID should not be undefined");
+        assert.equal(message.text, testText, "Message text should match");
+      } catch (error: any) {
+        assert.fail(`Error in sendMessage allow_sending_without_reply test: ${error.message}`);
+      }
+    });
+  });
+});
